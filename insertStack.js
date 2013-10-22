5c37b10d45eb0afa88c5e0ad9366406ae099c20b
anvaka <anvaka@gmail.com>
2013-10-22 10:19:30 -0700

    Extracted independent parts into own files

---
+++ insertStack.js
@@ -0,0 +1,43 @@
+module.exports = InsertStack;
+
+/**
+ * Our implmentation of QuadTree is non-recursive (recursion handled not really
+ * well in old browsers). This data structure represent stack of elemnts
+ * which we are trying to insert into quad tree. It also avoids unnecessary
+ * memory pressue when we are adding more elements
+ */
+function InsertStack () {
+    this.stack = [];
+    this.popIdx = 0;
+}
+
+InsertStack.prototype = {
+    isEmpty: function() {
+        return this.popIdx === 0;
+    },
+    push: function (node, body) {
+        var item = this.stack[this.popIdx];
+        if (!item) {
+            // we are trying to avoid memory pressue: create new element
+            // only when absolutely necessary
+            this.stack[this.popIdx] = new InsertStackElement(node, body);
+        } else {
+            item.node = node;
+            item.body = body;
+        }
+        ++this.popIdx;
+    },
+    pop: function () {
+        if (this.popIdx > 0) {
+            return this.stack[--this.popIdx];
+        }
+    },
+    reset: function () {
+        this.popIdx = 0;
+    }
+};
+
+function InsertStackElement(node, body) {
+    this.node = node; // QuadTree node
+    this.body = body; // physical body which needs to be inserted to node
+}
---
+++ node.js
@@ -0,0 +1,30 @@
+/**
+ * Internal data structure to represent 2D QuadTree node
+ */
+module.exports = function Node() {
+  // body stored inside this node. In quad tree only leaf nodes (by construction)
+  // contain boides:
+  this.body = null;
+
+  // Child nodes are stored in quads. Each quad is presented by number:
+  // 0 | 1
+  // -----
+  // 2 | 3
+  this.quads = [];
+
+  // Total mass of current node
+  this.mass = 0;
+
+  // Center of mass coordinates
+  this.massX = 0;
+  this.massY = 0;
+
+  // bounding box coordinates
+  this.left = 0;
+  this.top = 0;
+  this.bottom = 0;
+  this.right = 0;
+
+  // Node is internal when it is not a leaf
+  this.isInternal = false;
+};
